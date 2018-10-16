92133630859937a25d025251ab9c58d02c7b337d
ky0jr <dennymarcoalbert@gmail.com>
2018-10-16 22:43:04 +0700

    First Commit
    Dibuat dengan Unity 2018 .2.12

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null; //Static instance of GameManager which allows it to be accessed by any other script.
+
+    public InteracableObject[] interacableObject;
+
+    public bool interact = false;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    public string FindObject(string ObjectName)
+    {
+        string description = "";
+        for(int i = 0;i < interacableObject.Length; i++)
+        {
+            if (interacableObject[i]._name == ObjectName)
+                description = interacableObject[i].description;
+        }
+        return description;
+    }
+
+}
---
+++ Assets/Scripts/InteracableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Object", menuName = "InteracableObject")]
+public class InteracableObject : ScriptableObject {
+
+    public string _name = "name";
+
+    [TextArea]
+    public string description = "";
+	
+}
